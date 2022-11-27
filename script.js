d68437992913ad5902b089dc1e88b240a3971d19
Kyle Groening <kyle.groening@cytiva.com>
2022-11-27 15:44:56 -0500

    Add JS to populate HTML with operator buttons

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Array containing all operators
+const operators = ["+","-","*", "/"]
 
+// Populate HTML with buttons for all operators
+const operatorDiv = document.querySelector(".operators");
+operators.forEach(element => {
+    const opBtn = document.createElement('button');
+    opBtn.classList.add(`operator-button`);
+    opBtn.textContent = element;
+    operatorDiv.appendChild(opBtn);
+});
 
 function add(first,second) {
 	return first + second
