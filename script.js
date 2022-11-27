907313920304e58a0bf579306c2cd56e5b2603fb
Kyle Groening <kyle.groening@cytiva.com>
2022-11-27 15:49:59 -0500

    Reformat document

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array containing all operators
-const operators = ["+","-","*", "/", "="]
+const operators = ["+", "-", "*", "/", "="]
 
 // Populate HTML with buttons for all operators
 const operatorDiv = document.querySelector(".operators");
@@ -12,37 +12,37 @@ operators.forEach(element => {
 
 // Populate HTML with buttons for all numbers 0-9
 const numberDiv = document.querySelector(".numbers")
-for (let number = 0; number <= 9 ; number++) {
+for (let number = 0; number <= 9; number++) {
     const numBtn = document.createElement('button');
     numBtn.classList.add(`number-button`);
     numBtn.textContent = number;
     numberDiv.appendChild(numBtn);
 }
 
-function add(first,second) {
-	return first + second
+function add(first, second) {
+    return first + second
 };
 
-function subtract(first,second) {
-	return first - second
+function subtract(first, second) {
+    return first - second
 };
 
 function multiply(array) {
     let intProduct = array[0];
     for (let index = 1; index < array.length; index++) {
-      intProduct *= array[index]
+        intProduct *= array[index]
     }
     return intProduct
 };
 
-function divide(array){
+function divide(array) {
     let intQuotient = array[0];
     for (let index = 1; index < array.length; index++) {
-      intQuotient /= array[index]
+        intQuotient /= array[index]
     }
     return intQuotient
 }
 
 function power(number, toThePowerOf) {
-	return number ** toThePowerOf
+    return number ** toThePowerOf
 };
